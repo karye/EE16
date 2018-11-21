8af3d4fd0f01ab0b2778f3f4106d400993e599db
Karye <karye@users.noreply.github.com>
2018-11-21 13:21:25 +0100

    Tränat på formulär, sanitize och filhantering

--- htdocs/texteditor/texteditor.php
+++ htdocs/texteditor/texteditor.php
@@ -19,32 +19,46 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>En texteditor</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootswatch/4.1.3/flatly/bootstrap.min.css">
-    <link rel="stylesheet" href="./texteditor.css">
+    <link rel="stylesheet" href="texteditor.css">
+    <link rel="stylesheet" href="modal.css">
 </head>
 <body>
     <?php
-    $texten = "";
+$texten = "";
 
-    /* Kolla att data finns */
-    if (isset($_POST['texten'])) {
-        /* Validera data mot skräp och hot */
-        $texten = filter_input(INPUT_POST, 'texten', FILTER_SANITIZE_STRING);
+/* Kolla att data finns */
+if (isset($_POST['texten'])) {
+    /* Validera data mot skräp och hot */
+    $texten = filter_input(INPUT_POST, 'texten', FILTER_SANITIZE_STRING);
+    
+    /* Spara texten i en texfil */
+    $handtag = fopen('texten.txt', 'w') or die('<p>Kunde inte spara textfilen.</p>');
+    fwrite($handtag, $texten);
+    fclose($handtag);
+    
+    /* Öppna ett modal-fönstret för att bekräfta sparandet */
+    echo "<script defer src=\"modal.js\"></script>";
 
-        /* Spara texten i en texfil */
-        $handtag = fopen('texten.txt', 'w');
-        fwrite($handtag, $texten);
-        fclose($handtag);
-    } else {
-        /* Hämta texten från filen */
-        
-    }
-    ?>
+} else {
+    /* Hämta texten från filen */
+    $texten = file_get_contents('texten.txt');
+}
+?>
+    <!-- The Modal -->
+    <div id="myModal" class="modal">
+        <!-- Modal content -->
+        <div class="modal-content">
+            <div class="modal-body">
+                <p>Texten är sparad!</p>
+            </div>
+        </div>
+    </div>
     <div class="kontainer">
         <h1>Texteditor</h1>
         <form action="#" method="post">
             <label for="texten">Din text</label>
             <textarea name="texten" id="texten" cols="50" rows="10"><?php echo $texten; ?></textarea><br>
-            <button>Spara</button>
+            <button id="myBtn">Spara</button>
         </form>
     </div>
 </body>
