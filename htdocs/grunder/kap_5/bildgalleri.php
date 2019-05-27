4421c233583c9576cc692c5d20017b8c16003e3b
Karim Ryde <karim.ryde@learnet.se>
2019-05-27 13:45:49 +0200

    Snyggar till och städar

--- htdocs/grunder/kap_5/bildgalleri.php
+++ htdocs/grunder/kap_5/bildgalleri.php
@@ -13,29 +13,29 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Lista alla filer i en katalog</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../style.css">
 </head>
 <body>
-    <?php
-/* Ange sökväg till katalogen */
-$sokvag = "./bilder";
-
-/* Skanna katalogen */
-$filer = scandir($sokvag);
-
-echo "<div class=\"kontainer\">\n";
-echo "<h1>Bildgalleri</h1>\n";
-
-foreach ($filer as $fil) {
-    if ($fil != '.' && $fil != '..') {
-        echo "<div class=\"ros\">\n";
-        echo "<img class=\"ram vanster\" src=\"./bilder/$fil\" alt=\"Bild från unsplash.com\">\n";
-        echo "<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>\n";
-        echo "<hr>\n";
-        echo "</div>\n";
+    <div class="kontainer">
+        <h1>Bilder från <a href="https://unsplash.com">unsplash.com</a></h1>
+        <?php
+    /* Ange sökväg till katalogen */
+    $sokvag = "./bilder";
+    
+    /* Skanna katalogen */
+    $filer = scandir($sokvag);
+    
+    foreach ($filer as $fil) {
+        if ($fil != '.' && $fil != '..') {
+            echo "<div class=\"bild\">\n";
+            echo "<img class=\"ram vanster\" src=\"./bilder/$fil\" alt=\"Bild från unsplash.com\">\n";
+            echo "<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>\n";
+            echo "<hr>\n";
+            echo "</div>\n";
+        }
     }
-}
-echo "</div>\n";
-?>
+    echo "</div>\n";
+    ?>
+    </div>
 </body>
 </html>
--- htdocs/grunder/kap_5/filuppladdning.php
+++ htdocs/grunder/kap_5/filuppladdning.php
@@ -14,72 +14,76 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Filuppladdning</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/4.3.1/flatly/bootstrap.min.css">
+    <link rel="stylesheet" href="../style.css">
 </head>
 <body>
-<?php
-/* Kolla att man har klickat på knappen 'submit' */
-if (isset($_POST['submit'])) {
-    $filen =  $_FILES['filen'];
-
-    /* Plocka ut filnamnet */
-    $fileName = $filen['name'];
-
-    /* Plocka ut filtypen */
-    $fileType = $filen['type'];
-
-    /* Plocka ut filtypen */
-    $fileTempName = $filen['tmp_name'];
-
-    /* Plocka ut filstorleken */
-    $fileSize = $filen['size'];
-
-    /* Plocka ut felmeddelande */
-    $fileError = $filen['error'];
-
-    /* Plocka ut filändelse */
-    $fileExt = explode('image/', $fileType);
-
-    /* Tillåtna filtyper att laddat upp */
-    $allowedType = ['jpeg', 'png', 'gif', 'pdf'];
-
-    /* Felmeddelanden */
-    $errors = array(
-        1 => 'The uploaded file exceeds the upload_max_filesize directive in php.ini.',
-        2 => 'The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form.',
-        3 => 'The uploaded file was only partially uploaded.',
-        4 => 'No file was uploaded.',
-        6 => 'Missing a temporary folder.',
-        7 => 'Failed to write file to disk.',
-        8 => 'A PHP extension stopped the file upload.',
-    );
-
-    /* Är filen tillåten att ladda upp? */
-    if (in_array($fileExt[1], $allowedType)) {
-
-        /* Nästa steg - blev något fel? */
-        if ($fileError == 0) {
-            /* Skapa nytt unikt filnamn för att inte skriva filer med samma namn */
-            $fileNewName = uniqid('', true) . '.' . $fileExt[1];
-
-            /* Hela sökvägen till den nya filen */
-            $fileDestination = "./bilder/$fileNewName";
-
-            /* Flytta filen rätt */
-            move_uploaded_file($fileTempName, $fileDestination);
-            echo "<p>Uppladdning lyckades!</p>";
+    <div class="kontainer">
+        <?php
+    /* Kolla att man har klickat på knappen 'submit' */
+    if (isset($_POST['submit'])) {
+        $filen =  $_FILES['filen'];
+    
+        /* Plocka ut filnamnet */
+        $fileName = $filen['name'];
+    
+        /* Plocka ut filtypen */
+        $fileType = $filen['type'];
+    
+        /* Plocka ut filtypen */
+        $fileTempName = $filen['tmp_name'];
+    
+        /* Plocka ut filstorleken */
+        $fileSize = $filen['size'];
+    
+        /* Plocka ut felmeddelande */
+        $fileError = $filen['error'];
+    
+        /* Plocka ut filändelse */
+        $fileExt = explode('image/', $fileType);
+    
+        /* Tillåtna filtyper att laddat upp */
+        $allowedType = ['jpeg', 'png', 'gif', 'pdf'];
+    
+        /* Felmeddelanden */
+        $errors = array(
+            1 => 'The uploaded file exceeds the upload_max_filesize directive in php.ini.',
+            2 => 'The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form.',
+            3 => 'The uploaded file was only partially uploaded.',
+            4 => 'No file was uploaded.',
+            6 => 'Missing a temporary folder.',
+            7 => 'Failed to write file to disk.',
+            8 => 'A PHP extension stopped the file upload.',
+        );
+    
+        /* Är filen tillåten att ladda upp? */
+        if (in_array($fileExt[1], $allowedType)) {
+    
+            /* Nästa steg - blev något fel? */
+            if ($fileError == 0) {
+                /* Skapa nytt unikt filnamn för att inte skriva filer med samma namn */
+                $fileNewName = uniqid('', true) . '.' . $fileExt[1];
+    
+                /* Hela sökvägen till den nya filen */
+                $fileDestination = "./bilder/$fileNewName";
+    
+                /* Flytta filen rätt */
+                move_uploaded_file($fileTempName, $fileDestination);
+                echo "<p>Uppladdning lyckades!</p>";
+            } else {
+                echo "<p>Något gick fel: $errors[$fileError]</p>";
+            }
+            
         } else {
-            echo "<p>Något gick fel: $errors[$fileError]</p>";
+            echo "<p>Icke tillåten filtyp!</p>";
         }
-        
-    } else {
-        echo "<p>Icke tillåten filtyp!</p>";
     }
-}
-?>
-    <form action="upload.php" method="post" enctype="multipart/form-data">
-        <input type="file" name="filen">
-        <button type="submit" name="submit">Ladda upp!</button>
-    </form>
+    ?>
+        <h1>Ladda upp bilder</h1>
+        <form action="upload.php" method="post" enctype="multipart/form-data">
+            <input type="file" name="filen">
+            <button type="submit" name="submit">Ladda upp!</button>
+        </form>
+    </div>
 </body>
 </html>
