509320a3cbacf3aee6b5eb608510c52bb709b7e7
Karye <karye@users.noreply.github.com>
2020-04-20 10:16:08 +0200

    Fix

--- htdocs/övningar/kap_6/upg_6_1.php
+++ htdocs/övningar/kap_6/upg_6_1.php
@@ -20,62 +20,64 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <?php
-/* Kontrollera att data finns */
-if (isset($_POST["namn"]) && isset($_POST["adress"]) 
-&& isset($_POST["postnr"]) && isset($_POST["postort"])) {
-    
-    /* Ta emot data */
-    $namn = $_POST["namn"];
-    $adress = $_POST["adress"];
-    $postnr = $_POST["postnr"];
-    $postort = $_POST["postort"];
-    
-    /* Kontrollera att alla fälten är ifyllda */
-    if (strlen($namn) == 0) {
-        echo "<p>Varning tomt: Vg fyll i namnet.</p>";
+    <div class="kontainer">
+        <?php
+    /* Kontrollera att data finns */
+    if (isset($_POST["namn"]) && isset($_POST["adress"]) 
+    && isset($_POST["postnr"]) && isset($_POST["postort"])) {
+        
+        /* Ta emot data */
+        $namn = $_POST["namn"];
+        $adress = $_POST["adress"];
+        $postnr = $_POST["postnr"];
+        $postort = $_POST["postort"];
+        
+        /* Kontrollera att alla fälten är ifyllda */
+        if (strlen($namn) == 0) {
+            echo "<p>Varning tomt: Vg fyll i namnet.</p>";
+        }
+        if (strlen($adress) == 0) {
+            echo "<p>Varning tomt: Vg fyll i adressen.</p>";
+        }
+        if (strlen($postnr) == 0) {
+            echo "<p>Varning tomt: Vg fyll i postnr.</p>";
+        }
+        if (strlen($postort) == 0) {
+            echo "<p>Varning tomt: Vg fyll i postort.</p>";
+        }
+        
+        /* Kontrollera att alla fälten innehåller minst 3 tecken */
+        if (strlen($namn) < 3) {
+            echo "<p>Varning för kort: namnet måste vara minst tre tecken långt.</p>";
+        }
+        if (strlen($adress) < 3) {
+            echo "<p>Varning för kort: adressen måste vara minst tre tecken långt.</p>";
+        }
+        if (strlen($postnr) < 3) {
+            echo "<p>Varning för kort: postnr måste vara minst tre tecken långt.</p>";
+        }
+        if (strlen($postort) < 3) {
+            echo "<p>Varning för kort: postort måste vara minst tre tecken långt.</p>";
+        }
+        
+        /* Kontrollera att postnumret innehåller 5 tecken och att de tecknen endast är siffror */
+        if (strlen($postnr) != 5) {
+            echo "<p>Varning: Postnr måste vara 5 tecken lånt.</p>";
+        }
+        if (!ctype_digit($postnr)) {
+            echo "<p>Varning: Postnr får bara innehålla siffror.</p>";
+        }
+        
     }
-    if (strlen($adress) == 0) {
-        echo "<p>Varning tomt: Vg fyll i adressen.</p>";
-    }
-    if (strlen($postnr) == 0) {
-        echo "<p>Varning tomt: Vg fyll i postnr.</p>";
-    }
-    if (strlen($postort) == 0) {
-        echo "<p>Varning tomt: Vg fyll i postort.</p>";
-    }
-    
-    /* Kontrollera att alla fälten innehåller minst 3 tecken */
-    if (strlen($namn) < 3) {
-        echo "<p>Varning för kort: namnet måste vara minst tre tecken långt.</p>";
-    }
-    if (strlen($adress) < 3) {
-        echo "<p>Varning för kort: adressen måste vara minst tre tecken långt.</p>";
-    }
-    if (strlen($postnr) < 3) {
-        echo "<p>Varning för kort: postnr måste vara minst tre tecken långt.</p>";
-    }
-    if (strlen($postort) < 3) {
-        echo "<p>Varning för kort: postort måste vara minst tre tecken långt.</p>";
-    }
-    
-    /* Kontrollera att postnumret innehåller 5 tecken och att de tecknen endast är siffror */
-    if (strlen($postnr) != 5) {
-        echo "<p>Varning: Postnr måste vara 5 tecken lånt.</p>";
-    }
-    if (!ctype_digit($postnr)) {
-        echo "<p>Varning: Postnr får bara innehålla siffror.</p>";
-    }
-    
-}
-/* Formuläret för att mata in namn, adress, postnr och postort */
-?>
-    <form action="#" method="post">
-        <label>Namn</label><input type="text" name="namn"><br>
-        <label>Gatuadress</label><input type="text" name="adress"><br>
-        <label>Postnr</label><input type="text" name="postnr"><br>
-        <label>Postort</label><input type="text" name="postort"><br>
-        <button>Skicka in</button>
-    </form>
+    /* Formuläret för att mata in namn, adress, postnr och postort */
+    ?>
+        <form action="#" method="post">
+            <label>Namn</label><input type="text" name="namn"><br>
+            <label>Gatuadress</label><input type="text" name="adress"><br>
+            <label>Postnr</label><input type="text" name="postnr"><br>
+            <label>Postort</label><input type="text" name="postort"><br>
+            <button>Skicka in</button>
+        </form>
+    </div>
 </body>
 </html>
